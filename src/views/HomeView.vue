01233db262612d2eef6994ff74c699187422d6d3
Hamel Barrero <hamel_-barrero@hotmail.com>
2023-03-02 19:49:07 -0500

    clean code of init commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,7 @@
-<script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
+<script setup lang="ts"></script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <h2>Hello world</h2>
 </template>
+
+<style scoped></style>
